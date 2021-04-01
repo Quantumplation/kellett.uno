f9c78a04657635ad40225cb6d06e42521bb3cc0d
Pi Lanningham <pi.lanningham@gmail.com>
2021-03-31 23:59:50 -0400

    Unsaved file

--- src/store.ts
+++ src/store.ts
@@ -1,6 +1,4 @@
 import { Writable, writable } from "svelte/store";
-import type { GameEvent } from "./Model/event-sourcing";
 import type { Game } from "./Model/model";
 
-export let events: Writable<GameEvent[]> = writable([]);
 export let game: Writable<Game> = writable(null);
